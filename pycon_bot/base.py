a7bbb79e0495fab2215f119626d75cc8d5e05632
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-12-01 18:52:02 -0600

    Moved voter/nonvoter to the base bot.

--- pycon_bot/base.py
+++ pycon_bot/base.py
@@ -18,6 +18,28 @@ class BasePyConBot(irc.IRCClient):
     def nonvoter_list(self):
         return ', '.join(self.nonvoters) if self.nonvoters else 'none'
 
+    def handle_nonvoter(self, channel, *users):
+        users = set(users)
+        users.discard(self.nickname)
+        if not users:
+            self.msg(channel, "Nonvoters: %s." % self.nonvoter_list)
+            return
+        self.nonvoters.update(users)
+        self.msg(channel, "Will no longer pester %s." % ', '.join(users))
+
+    def handle_voter(self, channel, *users):
+        users = set(users)
+        users.discard(self.nickname)
+        if not users:
+            self.msg(channel, "Nonvoters: %s." % self.nonvoter_list)
+            return
+        if '*' in users:
+            self.nonvoters.clear()
+            self.msg(channel, "Will now pester everyone.")
+        else:
+            self.nonvoters.difference_update(users)
+            self.msg(channel, "Will now pester %s." % ', '.join(users))
+
     def handle_pester(self, channel):
         def names_callback(names):
             laggards = (set(names) - set(self.current_votes.keys()) -
--- pycon_bot/review.py
+++ pycon_bot/review.py
@@ -86,28 +86,6 @@ class PyConReviewBot(BasePyConBot):
             talk["id"]
         ))
 
-    def handle_nonvoter(self, channel, *users):
-        users = set(users)
-        users.discard(self.nickname)
-        if not users:
-            self.msg(channel, "Nonvoters: %s." % self.nonvoter_list)
-            return
-        self.nonvoters.update(users)
-        self.msg(channel, "Will no longer pester %s." % ', '.join(users))
-
-    def handle_voter(self, channel, *users):
-        users = set(users)
-        users.discard(self.nickname)
-        if not users:
-            self.msg(channel, "Nonvoters: %s." % self.nonvoter_list)
-            return
-        if '*' in users:
-            self.nonvoters.clear()
-            self.msg(channel, "Will now pester everyone.")
-        else:
-            self.nonvoters.difference_update(users)
-            self.msg(channel, "Will now pester %s." % ', '.join(users))
-
     def handle_vote(self, channel):
         self.clear_timer()
         self.current_votes = {}
