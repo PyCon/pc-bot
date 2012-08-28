2aa8967759b0eee544a675a9836233f39ee32efc
Jacob Kaplan-Moss <jacob@jacobian.org>
2012-08-28 11:18:45 -0500

    Made report.py report on a particular Meeting.

--- scripts/report.py
+++ scripts/report.py
@@ -1,45 +1,40 @@
 """
 Report on a meeting's decisions::
 
-    python report.py talks.json [first]
+    python report.py meeting_id
 
-Prints a summarized report of the votes on each talk starting at the talk with
-ID ``first``. If not given, sumarize all talks.
+Prints a summarized report of the votes on each talk decided in a meeting.
 
 Originally written by Luciano Ramalho and Steve Holden (see
-https://gist.github.com/1354185).
+https://gist.github.com/1354185), then updated over the years by JKM, Alex
+Gaynor, and others.
 """
 
+import argparse
+import pycon_bot.mongo
 import sys
-import json
+from pycon_bot.models import Meeting
 from textwrap import TextWrapper
 
+p = argparse.ArgumentParser()
+p.add_argument('--dsn')
+p.add_argument('meeting_id', type=int)
+args = p.parse_args()
+if not pycon_bot.mongo.connect(args.dsn):
+    sys.stderr.write("Need to pass --dsn or set env[MONGO_DSN].")
+    sys.exit(1)
+
 wrapper = TextWrapper(width=55)
+LINE_FORMAT = '{id:>3} {mark:>8} {yay:>3} {nay:>3} {abstain:>3}  {title}'
+print LINE_FORMAT.format(id='ID', mark='DECISION', yay='YAY', nay='NAY', abstain='ABS', title='TITLE')
 
-LINE_FORMAT = '{id:>3} {mark:>8} {yay:>3} {nay:>3} {abstain:>3}  {name}'
-
-with open(sys.argv[1]) as json_in:
-    talks = json.load(json_in)
-
-index = 0
-try:
-    first = int(sys.argv[2])
-except (IndexError, ValueError):
-    pass
-else:
-    while talks[index]['id'] != first:
-        index += 1
-
-print LINE_FORMAT.format(id='ID', mark='DECISION', yay='YAY', nay='NAY', abstain='ABS', name='TITLE')
-for talk in talks[index:]:
-    if 'decision' not in talk:
-        break
-    else:
-        mark = talk['decision'][0:6]
-        votes = talk['votes'] if 'votes' in talk else dict(yay=0, nay=0, abstain=0)
-        talk.update(votes)
-        namelist = wrapper.wrap(talk['name'])
-        talk['name'] = namelist[0]
-        print LINE_FORMAT.format(mark=mark, **talk)
-        for name in namelist[1:]:
-            print " "*25, name
+for t in Meeting.objects.get(number=args.meeting_id).talks_decided:
+    if t.status == 'unreviewed':
+        continue
+    mark = t.status[0:6]
+    v = t.kittendome_votes
+    titlelist = wrapper.wrap(t.title)
+    title = titlelist[0]
+    print LINE_FORMAT.format(id=t.talk_id, mark=mark, yay=v.yay, nay=v.nay, abstain=v.abstain, title=title)
+    for l in titlelist[1:]:
+        print " "*25, l
