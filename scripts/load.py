3b27d3bca6b2c127177d957d92379a32f7321a12
Jacob Kaplan-Moss <jacob@jacobian.org>
2012-08-28 10:19:14 -0500

    Added a --clear argument to the loading script.

--- scripts/load.py
+++ scripts/load.py
@@ -16,11 +16,15 @@ from pycon_bot.models import TalkProposal, SiteVotes
 p = argparse.ArgumentParser()
 p.add_argument('csvfile')
 p.add_argument('--dsn')
+p.add_argument('--clear', action='store_true', default=False)
 p.add_argument('-v', '--verbose', action='store_true', default=False)
 args = p.parse_args()
 if not pycon_bot.mongo.connect(args.dsn):
     sys.stderr.write("Need to pass --dsn or set env[MONGO_DSN].")
     sys.exit(1)
+
+if args.clear:
+    TalkProposal.objects.delete()
 
 for r in csv.DictReader(open(args.csvfile)):
     talk, created = TalkProposal.objects.get_or_create(talk_id=int(r['#']))
