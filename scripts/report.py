b9d30d3791e005edf8ae120d1442d45d5b0f8317
Jacob Kaplan-Moss <jacob@jacobian.org>
2012-08-29 11:52:43 -0500

    Added a few more flags and options to the report script.

--- scripts/report.py
+++ scripts/report.py
@@ -13,12 +13,13 @@ Gaynor, and others.
 import argparse
 import pycon_bot.mongo
 import sys
-from pycon_bot.models import Meeting
+from pycon_bot.models import Meeting, TalkProposal
 from textwrap import TextWrapper
 
 p = argparse.ArgumentParser()
 p.add_argument('--dsn')
-p.add_argument('meeting_id', type=int)
+p.add_argument('--all', action='store_true', default=False)
+p.add_argument('meeting_id', type=int, nargs='?')
 args = p.parse_args()
 if not pycon_bot.mongo.connect(args.dsn):
     sys.stderr.write("Need to pass --dsn or set env[MONGO_DSN].")
@@ -28,13 +29,22 @@ wrapper = TextWrapper(width=55)
 LINE_FORMAT = '{id:>3} {mark:>8} {yay:>3} {nay:>3} {abstain:>3}  {title}'
 print LINE_FORMAT.format(id='ID', mark='DECISION', yay='YAY', nay='NAY', abstain='ABS', title='TITLE')
 
-for t in Meeting.objects.get(number=args.meeting_id).talks_decided:
-    if t.status == 'unreviewed':
-        continue
+if args.meeting_id:
+    talks = Meeting.objects.get(number=args.meeting_id).talks_decided
+elif args.all:
+    talks = TalkProposal.objects.order_by('talk_id')
+else:
+    talks = TalkProposal.objects(status__ne='unreviewed').order_by('talk_id')
+
+for t in talks:
     mark = t.status[0:6]
     v = t.kittendome_votes
-    titlelist = wrapper.wrap(t.title)
+    if v:
+        y, n, a = v.yay, v.nay, v.abstain
+    else:
+        y = n = a = '-'
+    titlelist = wrapper.wrap(t.title.encode('utf-8'))
     title = titlelist[0]
-    print LINE_FORMAT.format(id=t.talk_id, mark=mark, yay=v.yay, nay=v.nay, abstain=v.abstain, title=title)
+    print LINE_FORMAT.format(id=t.talk_id, mark=mark, yay=y, nay=n, abstain=a, title=title)
     for l in titlelist[1:]:
         print " "*25, l
