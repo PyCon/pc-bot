73af1ea41a1fea601a09ca79c4335364e9dbd955
Jacob Kaplan-Moss <jacob@jacobian.org>
2012-08-30 16:11:15 -0500

    Updated agenda script to format the way Luke did it - I like that better.

--- scripts/agenda.py
+++ scripts/agenda.py
@@ -12,16 +12,25 @@ from pycon_bot.models import TalkProposal
 
 p = argparse.ArgumentParser()
 p.add_argument('--dsn')
-p.add_argument('-n', '--num', type=int, default=12)
+p.add_argument('-n', '--num', type=int, default=10)
+p.add_argument('-o', '--overflow', type=int, default=2)
 args = p.parse_args()
 if not pycon_bot.mongo.connect(args.dsn):
     sys.stderr.write("Need to pass --dsn or set env[MONGO_DSN].")
     sys.exit(1)
 
-nr = args.num
-for p in TalkProposal.objects(status='unreviewed').order_by('talk_id'):
-    if p.site_votes.total >= 3:
-        print '%s - %s' % (p.review_url, p.title)
-        nr -= 1
-    if nr == 0:
-        break
+talks = TalkProposal.objects(status='unreviewed').order_by('talk_id')[:args.num]
+overflow = TalkProposal.objects(status='unreviewed').order_by('talk_id')[args.num:args.num+args.overflow]
+
+def pt(t):
+    return "#%s - %s - %s\n%s\n" % (t.talk_id, t.title, t.speaker, t.review_url)
+
+print "=== AGENDA ==="
+print
+for t in talks:
+    print pt(t)
+if overflow:
+    print "=== OVERFLOW ==="
+    print
+    for t in overflow:
+        print pt(t)
