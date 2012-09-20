6d3615ad2b4b9c06e7d3d3cc48c8f7c8994352f4
Jacob Kaplan-Moss <jacob@jacobian.org>
2012-09-20 09:33:16 -0500

    Added --no-web and --no-irc flags so that you can choose which bits to run.

--- runbot.py
+++ runbot.py
@@ -15,10 +15,12 @@ from twisted.internet import reactor
 
 def run_bot(irc_server, irc_port, irc_channel, bot_name, http_port, logfile):
     log.startLogging(logfile)
-    bot = pycon_bot.driver.PyConBotFactory([irc_channel], bot_name)
-    web = WSGIResource(reactor, reactor.getThreadPool(), webapp)
-    reactor.connectTCP(irc_server, irc_port, bot)
-    reactor.listenTCP(http_port, Site(web))
+    if irc_server is not None:
+        bot = pycon_bot.driver.PyConBotFactory([irc_channel], bot_name)
+        reactor.connectTCP(irc_server, irc_port, bot)
+    if http_port is not None:
+        web = WSGIResource(reactor, reactor.getThreadPool(), webapp)
+        reactor.listenTCP(http_port, Site(web))
     reactor.run()
 
 if __name__ == '__main__':
@@ -28,21 +30,23 @@ if __name__ == '__main__':
     p.add_argument('--irc-channel', default=os.environ.get('PYCONBOT_CHANNEL', '#pycon-pc'))
     p.add_argument('--irc-nickname', default=os.environ.get('PYCONBOT_NICK', 'pycon_bot'))
     p.add_argument('--http-port', type=int, default=8000)
+    p.add_argument('--no-irc', dest='run_irc', action='store_false', default=True)
+    p.add_argument('--no-web', dest='run_web', action='store_false', default=True)
     p.add_argument('--dsn', default=None)
     args = p.parse_args()
-    
+
     # attempt to connect to the mongo db using either
     # the argument passed or the built-in environment variable
     if not pycon_bot.mongo.connect(args.dsn):
         sys.stderr.write("Need to pass --dsn or set env[MONGO_DSN].")
         sys.exit(1)
-        
+
     # Run ze bot!
     run_bot(
-        irc_server=args.irc_server,
+        irc_server=args.irc_server if args.run_irc else None,
         irc_port=args.irc_port,
         irc_channel=args.irc_channel,
         bot_name=args.irc_nickname,
-        http_port=args.http_port,
+        http_port=args.http_port if args.run_web else None,
         logfile=sys.stderr,
     )
