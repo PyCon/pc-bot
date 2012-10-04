608f26c56b69552d0144a73cdd8916d4d01ec114
Jacob Kaplan-Moss <jacob@jacobian.org>
2012-10-04 13:06:13 -0500

    Added a talks-by-status view (unlinked); spiffed up home page.

--- pycon_bot/web/app.py
+++ pycon_bot/web/app.py
@@ -1,9 +1,10 @@
-import os
-import math
-import flask
 import barrel.cooper
+import flask
+import json
+import math
 import mongoengine
 import mongoengine.queryset
+import os
 from datetime import datetime
 from flask.ext.bootstrap import Bootstrap
 from pycon_bot import mongo
@@ -28,6 +29,7 @@ def index():
     rejected = len(TalkProposal.objects(status__in=('rejected', 'posted')))
     number_of_meetings = len(Meeting.objects)
     talks_per_meeting = float(reviewed) / number_of_meetings
+    talks_by_status = TalkProposal.objects.item_frequencies('status').items()
     return flask.render_template('index.html',
         total = total,
         reviewed = reviewed,
@@ -39,7 +41,8 @@ def index():
         percent_rejected = (float(rejected) / reviewed) if reviewed else 0,
         number_of_meetings = number_of_meetings,
         talks_per_meeting = talks_per_meeting,
-        meetings_left = int(math.ceil(float(remaining) / talks_per_meeting))
+        meetings_left = int(math.ceil(float(remaining) / talks_per_meeting)),
+        talks_by_status_json = json.dumps(talks_by_status),
     )
 
 
@@ -60,22 +63,22 @@ def meeting_detail(n):
 @app.route('/talks')
 def talk_list():
     return flask.render_template('talk_list.html',
-        talks=TalkProposal.objects.order_by('talk_id'))
-
+        title = "All talks",
+        talks = TalkProposal.objects.order_by('talk_id'))
 
 @app.route('/talks/<int:n>')
 def talk_detail(n):
     """View returning detailed information about a talk."""
-    
+
     # set constants that should be outside the boundaries
     # of anything we ever actually care about
     LONG_TIME_AGO = datetime(1900, 1, 1, 0, 0, 0)
     LONG_TIME_FROM_NOW = datetime(2031, 12, 31, 23, 59, 59)
-    
+
     # retrieve the talk
     talk = get_or_404(TalkProposal.objects, talk_id=n)
     transcripts = []
-    
+
     # the transcripts are just stored by time, but I want
     #   to have some semblance of how to separate transcripts by
     #   various meetings
@@ -86,7 +89,7 @@ def talk_detail(n):
     meetings = list(Meeting.objects.all())
     cursor = -1
     dividing_line = LONG_TIME_AGO
-    
+
     # iterate over each line in the transcript and assess
     # where it belongs
     for line in talk.kittendome_transcript:
@@ -98,22 +101,30 @@ def talk_detail(n):
                 dividing_line = meetings[cursor].start
             else:
                 dividing_line = LONG_TIME_FROM_NOW
-                
+
             # also add a new list to `transcripts` so that the append
             # mechanism below hits the newest item
             if not len(transcripts) or len(transcripts[-1]):
                 transcripts.append([])
-                
+
         # at this point, we know that `transcripts` is a list of lists,
         # and that the last item in the list is where our line belongs,
         # so just shove it onto the stack
         transcripts[-1].append(line)
-    
+
     return flask.render_template('talk_detail.html',
         talk=talk,
         transcripts=transcripts,
     )
 
+@app.route('/talks/<string:status>')
+def talks_by_status(status):
+    statuses = dict(TalkProposal.STATUSES)
+    if status not in statuses:
+        flask.abort(404)
+    return flask.render_template('talk_list.html',
+        title = statuses[status],
+        talks = TalkProposal.objects.filter(status=status).order_by('talk_id'))
 
 def get_or_404(qs, *args, **kwargs):
     try:
